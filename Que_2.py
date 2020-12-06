3ef693255c9f308eaae7eb9a5c7d20144eb5268c
Ayush Muvel <63338883+Ayushmuvel@users.noreply.github.com>
2020-12-06 14:46:38 +0530

    Update Que_2.py
    column and row error

--- Que_2.py
+++ Que_2.py
@@ -1,5 +1,3 @@
-R = 3
-C = 5
 def ro_oth_or(temp,i,j,a):
     # change near by orange to rotten one initial to 3
     # then later on it will converted to 2
@@ -7,7 +5,7 @@ def ro_oth_or(temp,i,j,a):
         if temp[i-1][j] == 1:
             temp[i-1][j] = 3
             a = 1
-    if i+1 < R :
+    if i+1 < len(temp) :
         if temp[i+1][j] == 1:
             temp[i+1][j] = 3
             a = 1
@@ -15,7 +13,7 @@ def ro_oth_or(temp,i,j,a):
         if temp[i][j-1] == 1:
             temp[i][j-1] = 3
             a = 1
-    if j+1 < C :
+    if j+1 < len(temp[1]) :
         if temp[i][j+1] == 1:
             temp[i][j+1] = 3
             a = 1
@@ -35,8 +33,8 @@ def ch_roten_or (temp,ca):
         one = 0
         two = 0
         # count fresh and rotten orange
-        for i in range(R):
-            for j in range(C):
+        for i in range(len(temp)):
+            for j in range(len(temp[1])):
                 if temp[i][j] == 1:
                     one += 1
                 if temp[i][j] == 3 or temp[i][j] == 2:
@@ -46,8 +44,8 @@ def ch_roten_or (temp,ca):
     else :
         # when change take place in oranges
         # change the rotten orange vale 3 to 2 for standard form
-        for i in range(R):
-            for j in range(C):
+        for i in range(len(temp)):
+            for j in range(len(temp[1])):
                 if temp[i][j] == 3:
                     temp[i][j] = 2
         #print(temp)
@@ -58,12 +56,15 @@ def ch_roten_or (temp,ca):
 if __name__ == "__main__":
 
     v = [[2, 1, 0, 2, 1],
-		 [0, 0, 1, 2, 1],
-		 [1, 0, 0, 2, 1]]
+	 [1, 0, 1, 2, 1],
+	 [1, 0, 0, 2, 1],
+         [1, 0, 0, 0, 0],
+         [1, 1, 1, 1, 1],
+         [1, 0, 0, 0, 1],
+         [1, 0, 1, 0, 1]]
 
     res = ch_roten_or(v,0)
 
     print(f'time farmes: {res[0]}')
     print(f'fresh oranges: {res[1]}')
     print(f'Rotten oranges: {res[2]}')
-
